9891fc817185a43a7fcc18b2b38987fec8d2d5e8
Koske Kimutai <koskedk@live.com>
2018-07-13 08:02:50 +0300

    added AggregateRoot tests

---
+++ Sante.SharedKernel.Tests/Model/TestAggregateRootTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Sante.SharedKernel.Custom;
+using Sante.SharedKernel.Tests.TestArtifacts;
+
+namespace Sante.SharedKernel.Tests.Model
+{
+    [TestFixture]
+    public class TestAggregateRootTests
+    {
+        private TestAggregateRoot _testRoot;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testRoot = new TestAggregateRoot("Home test");
+        }
+
+        [Test]
+        public void should_have_a_LiveGuid()
+        {
+            Assert.False(_testRoot.Id.IsNullOrEmpty());
+            Console.WriteLine(_testRoot);
+        }
+    }
+}
---
+++ Sante.SharedKernel.Tests/TestArtifacts/TestAggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Sante.SharedKernel.Model;
+
+namespace Sante.SharedKernel.Tests.TestArtifacts
+{
+    public class TestAggregateRoot : AggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+
+        public TestAggregateRoot(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name}";
+        }
+    }
+}
